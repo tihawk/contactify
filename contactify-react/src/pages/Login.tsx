8514d001272bfec55f1158e91c3578fb26d0dd03
tihawk <cccp.hawk@gmail.com>
2023-10-17 12:42:01 +0300

    edited login page in frontend

--- contactify-react/src/pages/Login.tsx
+++ contactify-react/src/pages/Login.tsx
@@ -1,20 +1,51 @@
-import { Button, Typography } from '@mui/material';
+import { Button, Divider, FormControl, Grid, Input, InputLabel, Typography } from '@mui/material';
 import { useUser } from '../hooks/useUser';
+import { useState } from 'react';
 
 function Login() {
   const { changeUser } = useUser();
+  const [user, setUser] = useState('')
+  const [pass, setPass] = useState('')
 
   const handleLogin = () => {
     changeUser({
-      username: 'user',
-      password: 'user'
+      username: user,
+      password: pass
     });
   };
 
   return (
     <>
       <Typography variant="h2">Login</Typography>
-      <Button onClick={handleLogin}>Login</Button>
+      <Grid container spacing={2}>
+          <Grid item xs={12} md={12}>
+            <FormControl>
+              <InputLabel htmlFor="username">username</InputLabel>
+              <Input
+                id="username"
+                value={user}
+                onChange={e => setUser(e.target.value)}
+              />
+            </FormControl>
+          </Grid>
+          <Grid item xs={12} md={12}>
+            <FormControl>
+              <InputLabel htmlFor="password">password</InputLabel>
+              <Input
+                type="password"
+                id="password"
+                value={pass}
+                onChange={e => setPass(e.target.value)}
+              />
+            </FormControl>
+          </Grid>
+          <Grid item>
+            <Divider/>
+          </Grid>
+          <Grid item>
+            <Button onClick={handleLogin}>Login</Button>
+          </Grid>
+        </Grid>
     </>
   );
 };
